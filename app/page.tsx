3f8b209ce56eaa94026f12aaed2b2c5cee9c69e4
Justin Seawell <jpseawell@gmail.com>
2023-08-01 16:13:16 -0500

    feat: center content on the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import { Bio } from '@/components/Bio'
-import { Feed } from '@/components/Feed'
-import { posts } from '@/data/posts'
-import Image from 'next/image'
+import { Bio } from "@/components/Bio";
+import { Feed } from "@/components/Feed";
+import { posts } from "@/data/posts";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col p-4">
+    <main className="flex min-h-screen flex-col p-4 w-1/2 max-w-xl m-auto">
       <Bio />
       <Feed posts={posts} />
     </main>
-  )
+  );
 }
