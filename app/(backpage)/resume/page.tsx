8648e68c433706783796e94a630c1941f000a6db
Justin Seawell <jpseawell@gmail.com>
2023-10-16 21:15:00 -0500

    fix: start over with pages router instead

--- app/(backpage)/resume/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Resumé",
-};
-
-export default function ResumePage() {
-  return <section>Here's my resumé</section>;
-}
