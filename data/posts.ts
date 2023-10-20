f496abd682cf94cecbbb059bb3c838ce3b62bfbe
Justin Seawell <jpseawell@gmail.com>
2023-10-20 13:35:26 -0500

    feat: add async queue post

--- data/posts.ts
+++ data/posts.ts
@@ -14,4 +14,11 @@ export const posts: Post[] = [
     content:
       'Hello, welcome to my website. I post here about topics I\'m interested in, and projects I\'m working on. <br /><br />🔗 <a href="/about" class="link">About Me</a> <br />🔗 <a href="/resume" class="link">Resumé</a>',
   },
+  {
+    id: 2,
+    date: "2023-10-20T18:17:52.651Z",
+    pinned: false,
+    content:
+      "<p>I've been listening to <a href='https://twitter.com/ThePrimeagen' target='_blank' class='link'>@ThePrimeagen</a> a lot lately and he mentioned that no one he interviewed knew how to write an async queue in JS <i>(ok I know this is TS)</i>: <br /><br /><div style='height: 350px; overflow-y: auto; border-radius: 12px; border: 1px solid #ccc;'><script src='https://gist.github.com/jpseawell/14bab371122359143cc97686f9eda572.js'></script></div></p>",
+  },
 ];
