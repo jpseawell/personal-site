564cd8e6b605eb6d8362810e6d66070570ddc390
Justin Seawell <jpseawell@gmail.com>
2023-11-06 09:02:55 -0600

    feat: add github link to profile

--- components/profile.tsx
+++ components/profile.tsx
@@ -1,4 +1,6 @@
 import { content } from "@/data/content";
+import Image from "next/image";
+import githubSVG from "../public/github-mark-white.svg";
 
 export default function Profile() {
   return (
@@ -12,9 +14,24 @@ export default function Profile() {
         <div
           className={`bg-slate-800 rounded-full md:h-32 md:w-32 h-24 w-24 absolute top-16 md:top-16 left-4 border-2 border-slate-800 bg-[url('/avatar.jpg')] bg-contain bg-center shadow-md`}
         ></div>
+        <div className="text-right p-4">
+          <a
+            href="https://github.com/jpseawell"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="inline-block"
+              src={githubSVG}
+              alt="Justin's Github"
+              width={28}
+              height={28}
+            />
+          </a>
+        </div>
 
         {/* Profile Info */}
-        <div className="prose mt-12 md:mt-20 p-4 prose dark:prose-invert">
+        <div className="prose mt-0 md:mt-8 p-4 prose dark:prose-invert">
           <h2>{content.profileName}</h2>
           <p>{content.profileBio}</p>
         </div>
